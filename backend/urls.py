c93e85f7fd1a18ad897c540f1918e78a8b88a0d4
moreen <moreenvictor12@example.com>
2025-04-17 21:57:38 +0200

    new update

--- backend/urls.py
+++ backend/urls.py
@@ -1,20 +1,23 @@
 from django.urls import path
 from django.contrib import admin
-from api.editprofile import edit_profile  # استيراد الدالة edit_profile
-from api.recyclebag import create_recycle_bag, update_recycle_bag  # استيراد الدوال create_recycle_bag و update_recycle_bag
+from api.recyclebags import  get_recycle_bags ,create_recycle_bag, update_recycle_bag   # استيراد الدوال create_recycle_bag و update_recycle_bag
 from api.notifications import home_notify  # استيراد الدالة home_notify
 from api import editprofile
+from api import notifications
 urlpatterns = [
-    # مسارات API
-    path('api/edit-profile/', edit_profile, name='edit_profile'),
-    path('api/create-recycle-bag/', create_recycle_bag, name='create_recycle_bag'),
-    path('api/update-recycle-bag/', update_recycle_bag, name='update_recycle_bag'),
-   
-    # مسارات إضافية للإشعارات
-    path('api/home/notify/', home_notify, name='home_notify'),
+
     path('admin/', admin.site.urls),
     path('api/editprofile/', editprofile.edit_profile, name='edit_profile'),
     path('api/signup/', editprofile.sign_up, name='sign_up'),
     path('api/signin/', editprofile.sign_in, name='sign_in'),
     path('api/forgetpassword/', editprofile.forget_password, name='forget_password'),
+   #reyclebag
+    path('api/recyclebags/', get_recycle_bags, name='get_recycle_bags'),
+    path('api/recyclebags/create/', create_recycle_bag, name='create_recycle_bag'),
+    path('api/recyclebags/update/<int:pk>/', update_recycle_bag, name='update_recycle_bag'),
+
+#notifications
+    path('api/notifications/', notifications.get_notifications, name='get_notifications'),
+    path('api/notifications/create/', notifications.create_notification, name='create_notification'),
+  
 ]
