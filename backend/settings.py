427922d8c6a15affa69491be131d3fadb7f36dae
moreen <moreenvictor12@example.com>
2025-04-17 20:38:24 +0200

    updated

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',  # تأكد من أن api موجود هنا
+    'api', 
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
